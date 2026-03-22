06a7104356f09c42f7617ca4797e6982350a1a11
LikithReddy0409 <likithreddy0409@gmail.com>
2026-03-22 13:38:20 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,25 +3,30 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-/**
- * Hello world!
- *
- */
-public class App
-{
-    public static void main( String[] args )
-    {
-        WebDriver driver = new ChromeDriver();
+public class App {
+    public static void main(String[] args) {
 
-        /** driver.get("https://www.google.com/");
-        driver.manage().window().maximize(); **/
+        ChromeOptions options = new ChromeOptions();
+
+        // REQUIRED for Jenkins / Linux
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(options);
 
         driver.get("https://www.saucedemo.com/");
-        driver.manage().window().maximize();
 
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
+
+        System.out.println("Login successful");
+
+        driver.quit();
     }
 }
